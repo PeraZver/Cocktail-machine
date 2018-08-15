70e944d7f7c833548277c107e159bbb9ad5f5907
PeraZver <krivic.pero@gmail.com>
2018-08-15 21:31:34 +0200

    added cocktail scaling function

--- cocktail.py
+++ cocktail.py
@@ -24,8 +24,6 @@ def cleanAndExit():
 hx = HX711(5, 6)
 hx.set_reading_format("LSB", "MSB")
 hx.set_reference_unit(99.43)
-hx.reset()
-#hx.tare()
 
 ###################################
 # Setting up SSD detector
@@ -54,69 +52,66 @@ fps = FPS().start()
 person_detected = -5
 personCtr = 0
 
-def ShowAdd(personCtr):
-    """ Display the add depending on number of persons detected. """
-    if (personCtr):
-        print (str(personCtr) + " persons detected!")
-        #display the add
-        label = "{} persons detected".format(personCtr)
-        reklama_file = 'girl.jpg' if personCtr > 1 else 'wine.jpg'
-        reklama = cv2.imread(reklama_file)
-        cv2.namedWindow('reklama', cv2.WINDOW_NORMAL)
-        
-        screen_res = 1024, 760
-        scale_width = screen_res[0] / reklama.shape[1]
-        scale_height = screen_res[1] / reklama.shape[0]
-        scale = min(scale_width, scale_height)
-        window_width = int(reklama.shape[1] * scale)
-        window_height = int(reklama.shape[0] * scale)
-        cv2.resizeWindow('reklama', window_width, window_height)
-        
-        cv2.putText(reklama, label, (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 5, [255,255,255], 2)
-        # cv2.imshow('reklama',reklama)
-        
+def MakeACocktail(personCtr):
+	""" Make a cocktail depending on number of persons detected. """
+	if (personCtr):
+		print (str(personCtr) + " persons detected!")
+		print ("Giving you 3 sec to put a glass in the machine")
+		time.sleep(3)
+		# Reset the scale
+		hx.reset()
+		hx.tare()
+		start_time = time.time()
+		while (hx.get_weight(5) < 100 and (time.time() - start_time) < 5):
+			print ("Weight: ", hx.get_weight(5), " g")
+		hx.power_down()
+	
 # loop over the frames from the video stream
 while True:
-	# grab the frame from the threaded video stream and resize it
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+	try:
+		# grab the frame from the threaded video stream and resize it
+		frame = vs.read()
+		frame = imutils.resize(frame, width=400)
 
-	#if person is not detected proceed with detection algorithm
-	if ((time.time()- person_detected) > 5):
+		#if person is not detected proceed with detection algorithm
+		if ((time.time()- person_detected) > 5):
 
-		print("Going to detection mode")
-		# grab the frame dimensions and convert it to a blob
-		(h, w) = frame.shape[:2]
-		blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-			0.007843, (300, 300), 127.5)
+			print("Going to detection mode")
+			# grab the frame dimensions and convert it to a blob
+			(h, w) = frame.shape[:2]
+			blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+				0.007843, (300, 300), 127.5)
+			
+			# pass the blob through the network and obtain the detections and
+			# predictions
+			net.setInput(blob)
+			detections = net.forward()
+			print("Detected "+str(detections.shape[2])+" objects")
+			# find people with more than 75% confidence
+			personIdx = np.where(detections[0,0,:,1] == CLASSES.index("person")) # check how many detections are people
+			print("Detected " + str(personIdx[0].size)+" persons")
+			personConfidence = detections[0,0,personIdx[0],2] # find their confidences
+			print("Confidence: " + str(personConfidence))
+			personCtr = np.where(personConfidence > 0.75)[0].size	# return how many confidences are higher than 80%
+			print("pretty sure about " + str(personCtr) + " people")
+			person_detected = time.time() if personCtr > 0 else 0
+			# Make a cocktail
+			MakeACocktail(personCtr)
+
+
+		# show the output frame
+		#~ cv2.imshow("Frame", frame)
+		#key = cv2.waitKey(1) & 0xFF
 		
-		# pass the blob through the network and obtain the detections and
-		# predictions
-		net.setInput(blob)
-		detections = net.forward()
-		print("Detected "+str(detections.shape[2])+" objects")
-		# find people with more than 75% confidence
-		personIdx = np.where(detections[0,0,:,1] == CLASSES.index("person")) # check how many detections are people
-		print("Detected " + str(personIdx[0].size)+" persons")
-		personConfidence = detections[0,0,personIdx[0],2] # find their confidences
-		print("Confidence: " + str(personConfidence))
-		personCtr = np.where(personConfidence > 0.75)[0].size	# return how many confidences are higher than 80%
-		print("pretty sure about " + str(personCtr) + " people")
-		person_detected = time.time() if personCtr > 0 else 0
-		# Make a cocktail
-		ShowAdd(personCtr)
+		# if the `q` key was pressed, break from the loop
+		#if key == ord("q"):
+		#    break
+		
+		# update the FPS counter
+		fps.update()
 
-
-	# show the output frame
-	#~ cv2.imshow("Frame", frame)
-	#key = cv2.waitKey(1) & 0xFF
-	
-	# if the `q` key was pressed, break from the loop
-	#if key == ord("q"):
-	#    break
-	
-	# update the FPS counter
-	fps.update()
+	except (KeyboardInterrupt, SystemExit):
+		cleanAndExit()
 
 # stop the timer and display FPS information
 fps.stop()
